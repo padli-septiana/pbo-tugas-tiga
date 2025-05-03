065ca4e75000b29899c9385c438b5725b628a754
Septian Padli <muhammadpadliseptiana@upi.edu>
2025-05-03 21:26:04 +0700

    Initital commit

---
+++ src/main/java/com/mycompany/pbo/tugas/tiga/PboTugasTiga.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pbo.tugas.tiga;
+
+/**
+ *
+ * @author septianpadli
+ */
+public class PboTugasTiga {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
